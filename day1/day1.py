6724e8544ce93e8217cd2ca1aca564404efc5cc4
Adam Schneider <schneideradam@me.com>
2021-12-02 23:49:42 -0500

    forgot day 1

---
+++ day1/day1.py
@@ -0,0 +1,23 @@
+#!/bin/python
+
+NUMBERS = [int(i.strip('\n')) for i in open('input.txt', 'r').readlines()]
+WINDOW = 3
+
+def part_1():
+    count = 0
+    prev_num = float('inf')
+    for curr_num in NUMBERS:
+        count += int(curr_num > prev_num)
+        prev_num = curr_num
+    return count
+
+def part_2():
+    count = 0
+    prev_sum = float('inf')
+    for line in range(WINDOW, len(NUMBERS) + 1):
+        curr_sum = sum(NUMBERS[(line-WINDOW):line])
+    return curr_sum
+
+
+print(part_1())
+print(part_2())
