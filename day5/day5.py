c1dcd1b7d99e1eb6dccd6df925b3f09287548013
Adam Schneider <schneideradam@me.com>
2021-12-15 14:37:09 -0500

    day 5 part 2

--- day5/day5.py
+++ day5/day5.py
@@ -2,34 +2,54 @@
 
 import pandas as pd
 
-INPUT = [x.strip().split(" -> ") for x in open('lines_test.txt', 'r').readlines()]
+
+INPUT = [x.strip().split(" -> ") for x in open('lines.txt', 'r').readlines()]
 all_coordinates = list(map(lambda x: [list(map(int, x[0].split(","))), 
                                       list(map(int, x[1].split(",")))], INPUT))
 def part_1():
     def is_vertical_or_horizontal(coordinates):
         """
-        is it a vertical or horizontal line?
+        filter any invalid lines?
         """
         return (coordinates[0][0] == coordinates[1][0] or
-                coordinates[0][1] == coordinates[1][1])
+                coordinates[0][1] == coordinates[1][1] or
+                abs(coordinates[0][0] - coordinates [1][0]) == abs(coordinates[0][1] - coordinates [1][1])
+                )
 
     valid_coordinates = list(filter(is_vertical_or_horizontal, all_coordinates))
 
     dataframe = pd.DataFrame(0, (x for x in range(1001)), (x for x in range(1001)))
+
+    def _step(val, distance):
+        if distance < 0:
+            val -= 1
+            return val
+        else:
+            val += 1
+            return val
+
     for coordinates in valid_coordinates:
         coordinates.sort()
 
         x1, x2 = coordinates[0][0], coordinates[1][0]
         y1, y2 = coordinates[0][1], coordinates[1][1]
 
-        dataframe.iloc[x1:x2 + 1, y1:y2 + 1] += 1
+        distance_x = x2 - x1
+        distance_y = y2 - y1
 
+        if abs(distance_x) == abs(distance_y):
+            while x1 <= x2:
+                dataframe.iloc[x1, y1] += 1
+                x1 = _step(x1, distance_x)
+                y1 = _step(y1, distance_y)
+        else:
+            dataframe.iloc[x1:x2 + 1, y1:y2 + 1] += 1
+    
     # normalize and flag all overlapping an non-overlapping
     dataframe.mask(dataframe < 2, 0, inplace=True)
     dataframe.mask(dataframe >= 2, 1, inplace=True)
 
     print(dataframe.sum().sum())
-
 
 part_1()
 
